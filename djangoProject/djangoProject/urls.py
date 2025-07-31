b6fa765e2e1fa3f8e987a5679ab168892a05bff2
Marat Shavaleev <marat-shavaleev@mail.ru>
2025-07-31 11:13:34 +0500

    пятая глава без дубликата

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from horoscope import views
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('horoscope.urls')),
-
-]
+    #path('horoscope/', include('horoscope.urls')),
+] + debug_toolbar_urls()
